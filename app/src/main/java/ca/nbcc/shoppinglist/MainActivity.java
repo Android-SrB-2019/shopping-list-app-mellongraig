9cf54bb6335d9ac1d5dfb7eb8d11a667d1cc18be
mellongraig <mellongraig2@hotmail.com>
2019-02-09 11:38:57 -0400

    Second activity created

--- app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
+++ app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
@@ -1,13 +1,22 @@
 package ca.nbcc.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TEST_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pickNewItem(View view) {
+        Intent newItem = new Intent(this, SecondActivity.class);
+        startActivityForResult(newItem, TEST_REQUEST);
     }
 }
