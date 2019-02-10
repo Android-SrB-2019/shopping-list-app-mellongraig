02bbdf783be02f7c560d75da90c07fe4615f90e6
mellongraig <mellongraig2@hotmail.com>
2019-02-09 23:04:16 -0400

    Shopping List code created

--- app/src/main/java/ca/nbcc/shoppinglist/SecondActivity.java
+++ app/src/main/java/ca/nbcc/shoppinglist/SecondActivity.java
@@ -2,6 +2,7 @@ package ca.nbcc.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,4 +11,14 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
+
+    public void shopGroceryStoreItem(View view) {
+        switch (view.getId()) {
+            case R.id.add_rice:
+                break;
+        }
+
+    }
+
+
 }
