396e9c6870c1f9b0d84b2c9dfad4651122e08ac8
mellongraig <mellongraig2@hotmail.com>
2019-02-10 18:19:45 -0400

    Shopping list code updated

--- app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
+++ app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,30 @@ public class MainActivity extends AppCompatActivity {
     public static final String TOMATOES = "tomatoes";
     public static final String TUNA = "tuna";
     public static final String SALMON = "salmon";
+
+    //call global views
+    private TextView add_rice;
+    private TextView add_pasta;
+    private TextView add_apples;
+    private TextView add_oranges;
+    private TextView add_wheat;
+    private TextView add_flour;
+    private TextView add_cereal;
+    private TextView add_tomatoes;
+    private TextView add_tuna;
+    private TextView add_salmon;
+
+    //call global integers
+    private int rice = 0;
+    private int pasta = 0;
+    private int apples = 0;
+    private int oranges = 0;
+    private int wheat = 0;
+    private int flour = 0;
+    private int cereal = 0;
+    private int tomatoes = 0;
+    private int tuna = 0;
+    private int salmon = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +63,12 @@ public class MainActivity extends AppCompatActivity {
             if(requestCode == TEXT_REQUEST){
                 if(resultCode == RESULT_OK){
                     String shopping = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                    for(int i = 0; i < shopping.length; i++){
+                        if(shopping.contains(RICE)){
+                            rice += 1;
+                        }
+                    }
                 }
             }
         } catch(Exception e){
